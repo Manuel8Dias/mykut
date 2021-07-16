2fa32b9f8c422d1deb651471e354e51895e3fc4d
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-07-16 15:17:02 +0100

    Commit Aspecto 01

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,43 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
+import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+
+function ProfileSidebar(props) {
+  console.log(props);
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
+  const User = 'Manuel8Dias'
+
   return (
-    <MainGrid>
-      <div className='profileArea' style={{gridArea: 'profileArea'}}>
-        <Box>
-          <img src="https://github.com/Manuel8Dias.png" />
-        </Box>
-      </div>
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={User} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">
+              Bem vindo(a) 
+            </h1>
+          </Box>
+        </div>
 
-      <div className='welcomeArea' style={{gridArea: 'welcomeArea'}}>
-      <Box>
-        Bem-vindo(a) 
-      </Box>
+        <div className='profileRelationsArea' style={{gridArea: 'profileRelationsArea'}}>
+          <Box >
+            Pessoas da Comunidade
+          </Box>
+          <Box >
+            Comunidades
+          </Box>
       </div>
-
-      <div className='profileRelationsArea' style={{gridArea: 'profileRelationsArea'}}>
-        <Box >
-          Pessoas da Comunidade
-        </Box>
-        <Box >
-          Comunidades
-        </Box>
-      </div>
-    </MainGrid>
+      </MainGrid>
+    </>
   )
 }
