47408134b8070405f476879ffc4c5080635648c2
Manuel Dias <54215983+Manuel8Dias@users.noreply.github.com>
2021-07-16 14:04:07 +0100

    Commit componentes Box e MainGrid

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,29 @@
-import styled from 'styled-components'
-
-// const Title = styled.h1`
-//   font-size: 50px;
-//   color: ${({ theme }) => theme.colors.primary}
-// `
-
-const Box = styled.div`
-background: #fff;
-border-radius: 8px;
-`
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 
 export default function Home() {
   return (
-    <main>
-      <Box>
-        Imagem
-      </Box>
+    <MainGrid>
+      <div className='profileArea' style={{gridArea: 'profileArea'}}>
+        <Box>
+          <img src="https://github.com/Manuel8Dias.png" />
+        </Box>
+      </div>
+
+      <div className='welcomeArea' style={{gridArea: 'welcomeArea'}}>
       <Box>
         Bem-vindo(a) 
       </Box>
-      <Box>
-        Comunidades
-      </Box>
-    </main>
+      </div>
+
+      <div className='profileRelationsArea' style={{gridArea: 'profileRelationsArea'}}>
+        <Box >
+          Pessoas da Comunidade
+        </Box>
+        <Box >
+          Comunidades
+        </Box>
+      </div>
+    </MainGrid>
   )
 }
